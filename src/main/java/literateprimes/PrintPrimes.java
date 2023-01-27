2081bfc0e1ad8769e7a423225ddd869240c47aad
PureLeafGreen <volteczx8@gmail.com>
2023-01-27 14:14:37 -0500

    Changement de certaines variables
    Changement de variables evident

--- src/main/java/literateprimes/PrintPrimes.java
+++ src/main/java/literateprimes/PrintPrimes.java
@@ -2,14 +2,13 @@ package literateprimes;
 
 public class PrintPrimes {
   public static void main(String[] args) {
-    final int BANANE = 1000;
-    final int ROWPERPAGE = 50;
-    final int CC = 4;
-    final int WW = 10;
+    final int SECTION = 1000;
+    final int LIGNES_PAGE = 50;
+    final int COLONNES = 4;
 
     final int ORDMAX = 30;
-    int P[] = new int[BANANE + 1];
-    int PAGENUMBER;
+    int page[] = new int[SECTION + 1];
+    int nombre_page;
     int PAGEOFF;
     int ROWOFFSET;
     int C;
@@ -25,49 +24,49 @@ public class PrintPrimes {
 
     K2 = 1;
     K = 1;
-    P[1] = 2;
+    page[1] = 2;
     ORD = 2;
     SQUARE = 9;
 
-    while (K < BANANE) {
+    while (K < SECTION) {
       do {
         K2 = K2 + 2;
         // some comments
         if (K2 == SQUARE) {
           ORD = ORD + 1;
-          SQUARE = P[ORD] * P[ORD];
+          SQUARE = page[ORD] * page[ORD];
           MULT[ORD - 1] = K2;
         }
         N = 2;
         JPRIME = true;
         while (N < ORD && JPRIME) {
           while (MULT[N] < K2)
-            MULT[N] = MULT[N] + P[N] + P[N];
+            MULT[N] = MULT[N] + page[N] + page[N];
           if (MULT[N] == K2)
             JPRIME = false;
           N = N + 1;
         }
       } while (!JPRIME);
       K = K + 1;
-      P[K] = K2;
+      page[K] = K2;
     }
 
     {
-      PAGENUMBER = 1;
+      nombre_page = 1;
       PAGEOFF = 1;
-      while (PAGEOFF <= BANANE) {
-        System.out.println("The First " + BANANE +
-                           " Prime Numbers --- Page " + PAGENUMBER);
+      while (PAGEOFF <= SECTION) {
+        System.out.println("The First " + SECTION +
+                           " Prime Numbers --- Page " + nombre_page);
         System.out.println("");
-        for (ROWOFFSET = PAGEOFF; ROWOFFSET < PAGEOFF + ROWPERPAGE; ROWOFFSET++){
-          for (C = 0; C < CC;C++)
-            if (ROWOFFSET + C * ROWPERPAGE <= BANANE)
-              System.out.format("%10d", P[ROWOFFSET + C * ROWPERPAGE]);
+        for (ROWOFFSET = PAGEOFF; ROWOFFSET < PAGEOFF + LIGNES_PAGE; ROWOFFSET++){
+          for (C = 0; C < COLONNES;C++)
+            if (ROWOFFSET + C * LIGNES_PAGE <= SECTION)
+              System.out.format("%10d", page[ROWOFFSET + C * LIGNES_PAGE]);
           System.out.println("");
         }
         System.out.println("\f");
-        PAGENUMBER = PAGENUMBER + 1;
-        PAGEOFF = PAGEOFF + ROWPERPAGE * CC;
+        nombre_page = nombre_page + 1;
+        PAGEOFF = PAGEOFF + LIGNES_PAGE * COLONNES;
       }
     }
   }
