c9ae8e2dc0887dafc9d51dcc44e1a2070b06aa94
PureLeaf <volteczx8@gmail.com>
2023-01-30 13:40:14 -0500

    changement de variable pour prime_courant,nombre_courant,nombre_primes et le boolean pour est_prime

--- src/main/java/literateprimes/PrintPrimes.java
+++ src/main/java/literateprimes/PrintPrimes.java
@@ -2,71 +2,71 @@ package literateprimes;
 
 public class PrintPrimes {
   public static void main(String[] args) {
-    final int SECTION = 1000;
-    final int LIGNES_PAGE = 50;
-    final int COLONNES = 4;
+    final int NB_MAX_SECTION = 1000;
+    final int NB_LIGNES_PAGE = 50;
+    final int NB_COLONNES = 4;
 
     final int ORDMAX = 30;
-    int page[] = new int[SECTION + 1];
+    int page[] = new int[NB_MAX_SECTION + 1];
     int nombre_page;
     int PAGEOFF;
     int ROWOFFSET;
     int C;
-    int K2;
-    int K;
-    boolean JPRIME;
+    int prime_courant;
+    int nombre_courant;
+    boolean EST_PRIME;
 
-    // Vive les Mac
-    int ORD;
+    // Vive linux
+    int nombre_primes;
     int SQUARE;
     int N;
     int MULT[] = new int[ORDMAX + 1];
 
-    K2 = 1;
-    K = 1;
+    prime_courant = 1;
+    nombre_courant = 1;
     page[1] = 2;
-    ORD = 2;
+    nombre_primes = 2;
     SQUARE = 9;
 
-    while (K < SECTION) {
+    while (nombre_courant < NB_MAX_SECTION) {
       do {
-        K2 = K2 + 2;
+        prime_courant = prime_courant + 2;
         // some comments
-        if (K2 == SQUARE) {
-          ORD = ORD + 1;
-          SQUARE = page[ORD] * page[ORD];
-          MULT[ORD - 1] = K2;
+        if (prime_courant == SQUARE) {
+          nombre_primes = nombre_primes + 1;
+          SQUARE = page[nombre_primes] * page[nombre_primes];
+          MULT[nombre_primes - 1] = prime_courant;
         }
         N = 2;
-        JPRIME = true;
-        while (N < ORD && JPRIME) {
-          while (MULT[N] < K2)
+        EST_PRIME = true;
+        while (N < nombre_primes && EST_PRIME) {
+          while (MULT[N] < prime_courant)
             MULT[N] = MULT[N] + page[N] + page[N];
-          if (MULT[N] == K2)
-            JPRIME = false;
+          if (MULT[N] == prime_courant)
+            EST_PRIME = false;
           N = N + 1;
         }
-      } while (!JPRIME);
-      K = K + 1;
-      page[K] = K2;
+      } while (!EST_PRIME);
+      nombre_courant = nombre_courant + 1;
+      page[nombre_courant] = prime_courant;
     }
 
     {
       nombre_page = 1;
       PAGEOFF = 1;
-      while (PAGEOFF <= SECTION) {
-        System.out.println("The First " + SECTION +
+      while (PAGEOFF <= NB_MAX_SECTION) {
+        System.out.println("The First " + NB_MAX_SECTION +
                            " Prime Numbers --- Page " + nombre_page);
         System.out.println("");
-        for (ROWOFFSET = PAGEOFF; ROWOFFSET < PAGEOFF + LIGNES_PAGE; ROWOFFSET++){
-          for (C = 0; C < COLONNES;C++)
-            if (ROWOFFSET + C * LIGNES_PAGE <= SECTION)
-              System.out.format("%10d", page[ROWOFFSET + C * LIGNES_PAGE]);
+        for (ROWOFFSET = PAGEOFF; ROWOFFSET < PAGEOFF + NB_LIGNES_PAGE; ROWOFFSET++){
+          for (C = 0; C < NB_COLONNES;C++)
+            if (ROWOFFSET + C * NB_LIGNES_PAGE <= NB_MAX_SECTION)
+              System.out.format("%10d", page[ROWOFFSET + C * NB_LIGNES_PAGE]);
           System.out.println("");
         }
         System.out.println("\f");
         nombre_page = nombre_page + 1;
-        PAGEOFF = PAGEOFF + LIGNES_PAGE * COLONNES;
+        PAGEOFF = PAGEOFF + NB_LIGNES_PAGE * NB_COLONNES;
       }
     }
   }
